c500ce0b4c96696542a9695c9b66ff027cb3c07c
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2024-10-27 04:41:47 +0530

    added

--- server/index.js
+++ server/index.js
@@ -6,12 +6,16 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/userRoutes');
 connectToMongo();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
+// Root route (optional)
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
 //Available routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
