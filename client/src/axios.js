f4bfc61b0711b500e7b0cf148802cea5339041f7
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2024-10-28 00:41:55 +0530

    added

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:80/api',
+    baseURL: 'https://registration-page-server.vercel.app/api',
   });
   
   export default instance;
